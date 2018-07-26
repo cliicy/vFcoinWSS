b0ddddc46cda2646dcd9284030b70c11fd5f4bea
luojianjun <luojianjun@yingu.com>
2018-07-26 12:16:37 +0800

    arrage file format

---
+++ balance.py
@@ -0,0 +1,45 @@
+
+#!-*-coding:utf-8 -*-
+# @TIME    : 2018/7/26/0010 11:56
+# @Author  : Luo
+
+
+class Balance(object):
+
+    def __init__(self, available=0.0, frozen=0.0, balance=0.0):
+        """
+        :param available: avaiable
+        :type available: float
+        :param frozen: frozen
+        :type frozen: float
+        :param balance: balance
+        :type balance: float
+        :return:
+        """
+        self._available = available
+        self._frozen = frozen
+        self._balance = balance
+
+    @property
+    def available(self):
+        return self._available
+
+    @available.setter
+    def available(self, value):
+        self._available = value
+
+    @property
+    def frozen(self):
+        return self._frozen
+
+    @frozen.setter
+    def frozen(self, value):
+        self._frozen = value
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, value):
+        self._balance = value
---
+++ config.py
@@ -0,0 +1,52 @@
+
+#!-*-coding:utf-8 -*-
+# @TIME    : 2018/7/26/0011 10:17
+# @Author  : Luo
+
+# 秘钥
+key = '0000'
+secret = '0000'
+
+
+# 精度和最小挂单量
+btc = {'name': 'btcusdt', 'coin': 'btc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+bch = {'name': 'bchusdt', 'coin': 'bch', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+ltc = {'name': 'ltcusdt', 'coin': 'ltc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+eth = {'name': 'ethusdt', 'coin': 'eth', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+etc = {'name': 'etcusdt', 'coin': 'etc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+ft = {'name': 'ftusdt', 'coin': 'ft', 'price_precision': 6, 'amount_precision': 0, 'min_amount': 5}
+btm = {'name': 'btmusdt', 'coin': 'btm', 'price_precision': 4, 'amount_precision': 1, 'min_amount': 5}
+
+# 交易对，仅支持以上USDT交易对
+symbol = ft
+
+# 抱团模式：固定价格刷单(等于0就是市价，大于0固定该价格)
+fix_price = 0
+
+# 当固定价格有效时,买入前判断市价与固定价的差价，在范围帐内则下单
+diff_price = 0.02
+
+# ★买卖深度前3挂单数量总和
+total_amount = 0
+
+# 当★满足时的买入挂单量,如果梭哈刷设置个很大的值
+max_amount = 6
+
+# 当★不满足时的买入挂单量
+min_amount = 6
+
+# 持仓币种最大量
+limit_amount = 10
+
+# 暂时无用
+ft_base_amount = 0
+
+# 买单超时(s)
+delay = 20
+
+trade_head = ['amount', 'ts', 'id', 'side', 'price']
+# sylist = ['btcusdt', 'ethusdt', 'bchusdt', 'ltcusdt', 'ftusdt', 'fteth', 'etcusdt', 'ftbtc', 'bnbusdt', 'btmusdt']
+# sylist = ['btcusdt', 'bchusdt', 'ethusdt', 'ltcusdt']
+sylist = ['btcusdt']
+
+
