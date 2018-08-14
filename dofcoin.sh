0f9af41b72168d04d1ab7a86f22fff2300cba016
cliicy <cliicy@hotmail.com>
2018-08-14 20:25:12 +0800

    change ltusdt to ltcusdt

--- dofcoin.sh
+++ dofcoin.sh
@@ -4,7 +4,7 @@
 nohup python candle.py btcusdt > /dev/null 2>&1 &
 nohup python candle.py bchusdt > /dev/null 2>&1 &
 nohup python candle.py ethusdt > /dev/null 2>&1 &
-nohup python candle.py ltusdt > /dev/null 2>&1 &
+nohup python candle.py ltcusdt > /dev/null 2>&1 &
 nohup python ticker.py btcusdt > /dev/null 2>&1 &
 nohup python ticker.py bchusdt > /dev/null 2>&1 &
 nohup python ticker.py ethusdt > /dev/null 2>&1 &
@@ -12,7 +12,7 @@ nohup python ticker.py ltcusdt > /dev/null 2>&1 &
 nohup python depth.py btcusdt > /dev/null 2>&1 &
 nohup python depth.py bchusdt > /dev/null 2>&1 &
 nohup python depth.py ethusdt > /dev/null 2>&1 &
-nohup python depth.py ltusdt > /dev/null 2>&1 &
+nohup python depth.py ltcusdt > /dev/null 2>&1 &
 nohup python trade.py btcusdt > /dev/null 2>&1 &
 nohup python trade.py bchusdt > /dev/null 2>&1 &
 nohup python trade.py ethusdt > /dev/null 2>&1 &
